a64b85027f4380abdc6c3085b92d43bc8c375db5
Valdenir Severino <vald3nir@gmail.com>
2021-11-04 09:44:07 -0300

    set database config

--- database/__init__.py
+++ database/__init__.py
@@ -5,18 +5,16 @@ import pymongo
 
 # DB CONFIG
 _DATABASE = "smart_energy"
-# DB_USER_NAME = "dev"
-# DB_USER_PASSWORD = "njOcs9iCSBCO8Gro"
-# DB_HOST = "realmcluster.ikdg6.mongodb.net"
-#
-# _USER_ID = f"{DB_USER_NAME}:{DB_USER_PASSWORD}@{DB_HOST}"
-# _CLIENT_URL = f"mongodb+srv://{_USER_ID}/{_DATABASE}?retryWrites=true&w=majority"
+DB_USER_NAME = "dev"
+DB_USER_PASSWORD = "1721"
+DB_HOST = "127.0.0.1"
+_CLIENT_URL = f"mongodb://{DB_USER_NAME}:{DB_USER_PASSWORD}@{DB_HOST}"
 
 
 class MongoDB:
     def __init__(self, collection, ) -> None:
         super().__init__()
-        self.collection = pymongo.MongoClient()[_DATABASE][collection]
+        self.collection = pymongo.MongoClient(_CLIENT_URL)[_DATABASE][collection]
 
     def find_one(self, query):
         return self.collection.find_one(query)
@@ -24,8 +22,11 @@ class MongoDB:
     def update(self, query, new_value):
         self.collection.update_one(query, new_value, upsert=True)
 
-    def insert_one(self, query):
-        self.collection.insert_one(query)
+    def insert_one(self, document):
+        self.collection.insert_one(document=document)
+
+    def insert_many(self, documents):
+        self.collection.insert_many(documents=documents)
 
     def aggregate(self, query):
         self.collection.aggregate(query)
