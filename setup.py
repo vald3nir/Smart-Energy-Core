8a57ca8ebfc0cb411ba88cc086d263a66475a172
Valdenir Severino <vald3nir@gmail.com>
2021-11-09 11:24:59 -0300

    setup project

---
+++ setup.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# !/usr/bin/python3
+
+from subprocess import call
+
+libraries = ["pandas", "numpy", "pymongo[srv]", ]
+
+call("pip install " + ' '.join(libraries), shell=True)
+call("pip freeze > requirements.txt", shell=True)
