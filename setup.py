ef190070beaadc200886e0ed47af2b3028f36631
Valdenir Severino <vald3nir@gmail.com>
2021-11-12 14:12:08 -0300

    add plot library

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from subprocess import call
 
-libraries = ["pandas", "numpy", "pymongo[srv]", ]
+libraries = ["matplotlib", "pandas", "numpy", "pymongo[srv]"]
 
 call("pip install " + ' '.join(libraries), shell=True)
 call("pip freeze > requirements.txt", shell=True)
