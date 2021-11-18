f703c2932602ea8ca0963d5d81d791e431f6d1e9
Valdenir Severino <vald3nir@gmail.com>
2021-11-18 14:05:36 -0300

    update database

--- src/tasks/database/task_database_main.py
+++ src/tasks/database/task_database_main.py
@@ -3,12 +3,19 @@ from src.tasks import USERS_FOLDER
 from src.tasks.database.task_database_backup import backup_collection
 from src.tasks.database.task_database_restore import restore_collection
 
-if __name__ == '__main__':
 
+def run_restore_database():
     for user_name in utils_io.list_sub_folders(USERS_FOLDER):
         restore_collection(user_name)
-
     print("Database restored")
 
+
+def run_backup_collection():
     for user_name in utils_io.list_sub_folders(USERS_FOLDER):
         backup_collection(user_name)
+    print("Backup finished")
+
+
+if __name__ == '__main__':
+    # run_restore_database()
+    run_backup_collection()
