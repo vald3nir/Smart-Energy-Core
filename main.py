ee940d5c77d2b75f51479b908526323f598e925d
Valdenir Severino <vald3nir@gmail.com>
2022-02-22 14:49:24 -0300

    update main script

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# coding=utf-8
-# !/usr/bin/python3
-
 import src.utils.utils_io as utils_io
 from src.tasks import USERS_FOLDER
 from src.tasks.database.task_database_backup import backup_collection
@@ -9,9 +6,14 @@ from src.tasks.extract_data.task_check_duplicates import check_duplicates
 from src.tasks.extract_data.task_check_peaks import normalize_data_sets_peaks
 from src.tasks.extract_data.task_reduce_time_series import reduce_time_series
 from src.tasks.extract_data.task_validate_header import check_time_series_header
+from src.tasks.plot_data.task_plot_consumption_daily import plot_consumption_daily
+from src.tasks.plot_data.task_plot_consumption_montly import plot_consumption_monthly
+from src.tasks.plot_data.task_plot_consumption_yearly import plot_consumption_yearly
+from src.tasks.plot_data.task_plot_last_consumption_days import plot_last_consumption_days
+from src.tasks.plot_data.task_plot_last_consumption_months import plot_last_consumption_months
 
-if __name__ == "__main__":
 
+def extract_data():
     print("Step 0: move the time series to cache")
     for user_name in utils_io.list_sub_folders(USERS_FOLDER):
         cache_time_series(user_name)
@@ -35,3 +37,17 @@ if __name__ == "__main__":
     print("Step 5: database backup")
     for user_name in utils_io.list_sub_folders(USERS_FOLDER):
         backup_collection(user_name)
+
+
+def plot_data():
+    _user_name = "Valdenir"
+    plot_consumption_daily(_user_name=_user_name, _date="2021-11-18")
+    plot_consumption_monthly(_user_name=_user_name, _date="2021-10")
+    plot_consumption_yearly(_user_name=_user_name, _year="2021")
+    plot_last_consumption_days(_user_name=_user_name, num_days=5)
+    plot_last_consumption_months(_user_name, num_months=24)
+
+
+if __name__ == "__main__":
+    extract_data()
+    # plot_data()
