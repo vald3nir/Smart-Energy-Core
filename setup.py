a02977d133f8d58a000461ebbd46bba3183f5b14
Valdenir Severino <vald3nir@gmail.com>
2022-02-26 10:41:48 -0300

    update libraries

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
-from src.utils.setup_utils import load_libraries
+from subprocess import call
+
+import src.utils.utils_io as utils_io
+
+
+def load_libraries(libraries):
+    call("pip install " + ' '.join(libraries), shell=True)
+    # call("pip freeze > requirements.txt", shell=True)
+    utils_io.write_array("requirements.txt", [l + "\n" for l in libraries])
+
 
 if __name__ == '__main__':
     load_libraries(libraries=[
         "setuptools", "wheel",
         "matplotlib", "pandas", "numpy",
-        "pymongo[srv]"
+        "pymongo[srv]", "pyrebase4",
+        "pillow", "qrcode"
     ])
