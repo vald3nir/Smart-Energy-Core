86d59701fdf48a46c9eb3df30696dc6f0b61035b
Valdenir Severino <vald3nir@gmail.com>
2022-03-02 17:27:01 -0300

    fix imports

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from src.tasks.database_task import export_data_to_firebase
 from src.tasks.device_task import create_new_user_with_device
+from src.tasks.export_firebase_task import export_data_to_firebase
 from src.tasks.extract_data_task import extract_data
 
 if __name__ == '__main__':
