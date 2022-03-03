65d0137ebd8783f5741e93f90fdcafd84d2b04bb
Valdenir Severino <vald3nir@gmail.com>
2022-03-03 07:33:57 -0300

    export data consumption to firebase

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from src.tasks.extract_data_task import extract_data
 if __name__ == '__main__':
     # create_new_user_with_device(["new_user"])
     extract_data()
-    # export_data_to_firebase()
+    export_data_to_firebase()
     print("Finish!")
