a94bfa9f0b6668a5d50d4799806b68040fa57c1e
Valdenir Severino <vald3nir@gmail.com>
2022-03-26 15:46:26 -0300

    print database url

--- src/database/MongoDB.py
+++ src/database/MongoDB.py
@@ -7,8 +7,8 @@ _DEFAULT_DATABASE = "EnergyConsumption"
 _DB_HOST = f"smartenergycluster.jsanh.mongodb.net/{_DEFAULT_DATABASE}?retryWrites=true&w=majority"
 _CLIENT_URL = f"mongodb+srv://{_DB_USER_NAME}:{_DB_USER_PASSWORD}@{_DB_HOST}"
 
-
-# print("MongoDB:", _CLIENT_URL)
+# if __name__ == '__main__':
+#     print("MongoDB:", _CLIENT_URL)
 
 
 class _MongoDBRemote:
@@ -31,7 +31,7 @@ class _MongoDBRemote:
     def aggregate(self, pipeline):
         return self.collection.aggregate(pipeline=pipeline)
 
-    def find_all(self, query={}, sort_field='_id'):      
+    def find_all(self, query={}, sort_field='_id'):
         return list(self.collection.find(query).sort([(sort_field, pymongo.ASCENDING)]))
 
     def last(self, limit=5):
