1be07579df951ffdbe4349758606b2bc2550f5a3
Valdenir Severino <vald3nir@gmail.com>
2022-04-14 10:39:05 -0300

    update install libraries

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+from subprocess import call
+
+import src.utils.utils_io as utils_io
 from src.tasks.database_task import restore_collections
 from src.tasks.device_task import create_new_user_with_device
 from src.tasks.export_firebase_task import export_data_to_firebase
 from src.tasks.extract_data_task import extract_data
 
+
+def install_libraries():
+    call("pip install " + ' '.join(["--upgrade pip", "setuptools", "wheel"]), shell=True)
+
+    libraries = ["matplotlib", "pandas", "numpy", "pymongo[srv]", "firebase-admin", "pillow", "qrcode", ]
+    call("pip install " + ' '.join(libraries), shell=True)
+    utils_io.write_array("requirements.txt", [library + "\n" for library in libraries])
+
+
 if __name__ == '__main__':
+    # install_libraries()
     # restore_collections()
     # create_new_user_with_device(["new_user"])
     extract_data()
