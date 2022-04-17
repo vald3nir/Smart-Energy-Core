d30f53736d68050d4e60e17fe02aa08ebcf65b61
Valdenir Severino <vald3nir@gmail.com>
2022-04-17 10:18:04 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from subprocess import call
+
+import src.utils.utils_io as utils_io
+
+
+def install_libraries():
+    call("pip install " + ' '.join(["--upgrade pip", "setuptools", "wheel"]), shell=True)
+
+    libraries = ["matplotlib", "pandas", "numpy", "pymongo[srv]", "firebase-admin", "pillow", "qrcode", ]
+    call("pip install " + ' '.join(libraries), shell=True)
+    utils_io.write_array("requirements.txt", [library + "\n" for library in libraries])
+
+
+if __name__ == '__main__':
+    install_libraries()
