b2ec9631d230cb7bcd0ff9a0ae0a4d70dd5c4f68
Valdenir Severino <vald3nir@gmail.com>
2022-04-23 16:15:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+from src.tasks.export_firebase_task import export_data_to_firebase
 from src.tasks.extract_data_task import extract_data
 
 if __name__ == '__main__':
-    # restore_collections()
-    # create_new_user_with_device(["new_user"])
     extract_data()
-    # export_data_to_firebase()
+    export_data_to_firebase()
     print("Finish!")
